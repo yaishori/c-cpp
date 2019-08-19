1bddafb23a60913b7c40b5fa382b12c88f9b3fd0
ori yaish <yaishu@post.bgu.ac.il>
2019-08-19 09:21:15 +0300

    Add files via upload

--- bit operations/bitOpTest.c
+++ bit operations/bitOpTest.c
@@ -24,56 +24,52 @@ int main(){
 		scanf("%d", &option);
 		switch (option) {
 			case 1:
-				printf("enter char\n");
-				scanf("%hhu",&c);
-				printf("before  \n");
-				printBin(c);
-				c=invertBits(c);
-				printf("after \n");
-				printBin(c);
-				break;
+					c='a';
+					printf("before  \n");
+					printBin(c);
+					c=invertBits(c);
+					printf("after \n");
+					printBin(c);
+					break;
 			case 2:
-				printf("enter char\n");
-				scanf("%hhu",&c);
-				printf("enter num to rotate\n");
-				scanf("%d",&num);
-				printf("before  \n");
-				printBin(c);
-				c=rotateRight(c,x);
-				printf("after \n");
-				printBin(c);
-				break;
+					c='a';
+					printf("enter num to rotate\n");
+					scanf("%d",&num);
+					printf("before  \n");
+					printBin(c);
+					c=rotateRight(c,num);
+					printf("after \n");
+					printBin(c);
+					break;
 			case 3:
-				printf("enter char\n");
-				scanf("%hhu",&c);
-				printf("enter second char\n");
-				scanf("%hhu",&c2);
-				printf("enter p\n");
-				scanf("%d",&p);
-				printf("enter n\n");
-				scanf("%d",&n);
-				printf("before  \n");
-				printBin(c);
-				c=setbits(c,p,n,c2);
-				printf("after \n");
-				printBin(c);
-				break;
+					c='a';
+					c2='c';
+					printf("enter p\n");
+					scanf("%d",&p);
+					printf("enter n\n");
+					scanf("%d",&n);
+					printf("before  \n");
+					printBin(c);
+					c=setbits(c,p,n,c2);
+					printf("after \n");
+					printBin(c);
+					break;
 			case 4:
-				printf("enter int\n");
-				scanf("%u",&x);
-				printf("enter 1 or 0 for value\n");
-				scanf("%d",&num);
-				printf("enter from index\n");
-				scanf("%d",&p);
-				printf("enter to index\n");
-				scanf("%d",&n);
-				printf("before  \n");
-				int2bin(x);
-				setItoJ(x,p,n,num);
-				break;
+					printf("enter int\n");
+					scanf("%u",&x);
+					printf("enter 1 or 0 for value\n");
+					scanf("%d",&num);
+					printf("enter from index\n");
+					scanf("%d",&p);
+					printf("enter to index\n");
+					scanf("%d",&n);
+					printf("before  \n");
+					int2bin(x);
+					setItoJ(x,p,n,num);
+					break;
 			default:
-				cont = 0;
-				break;
+					cont = 0;
+					break;
 		}
 	}
 
